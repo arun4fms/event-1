4794af625d07fec9e7b95cbc08e5e12dd33287b0
Bima Wijaya <bimajatiwijaya@gmail.com>
2017-12-07 13:20:53 +0700

    [ADD] Data user contract and unit test.

---
+++ app/src/main/java/com/app/main/event/data/DataUser.java
@@ -0,0 +1,16 @@
+package com.app.main.event.data;
+
+/**
+ * Created by bima on 07/12/17.
+ */
+import android.provider.BaseColumns;
+
+
+public class DataUser {
+    public static final class DataUserEntry implements BaseColumns {
+        public static final String TABLE_NAME = "user";
+        public static final String COLUMN_USERNAME = "username";
+        public static final String COLUMN_EMAIL = "email";
+        public static final String COLUMN_PASSWORD = "password";
+    }
+}
