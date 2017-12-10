37e195c7ba2b3327e9c171c8246ad95e5c88bccd
Bima Wijaya <bimajatiwijaya@gmail.com>
2017-12-10 12:02:24 +0700

    [FIX] Unit test failed after add 1 column.

--- app/src/test/java/com/app/main/event/DataUserClassUnitTest.java
+++ app/src/test/java/com/app/main/event/DataUserClassUnitTest.java
@@ -40,7 +40,7 @@ public class DataUserClassUnitTest {
         assertEquals("Cannot find inner class to complete unit test", 1, innerClasses.length);
         Class entryClass = innerClasses[0];
         Field[] allFields = entryClass.getDeclaredFields();
-        assertEquals("There should be exactly 4 String members in the inner class", 4, allFields.length);
+        assertEquals("There should be exactly 5 String members in the inner class", 5, allFields.length);
         for (Field field : allFields) {
             assertTrue("All members in the contract class should be Strings", field.getType()==String.class);
             assertTrue("All members in the contract class should be final", Modifier.isFinal(field.getModifiers()));
